c092d03fe798eba2abb90b5cd85e925cf5fb0411
Alain Anghelidi <alainanghelidi@gmail.com>
2023-07-15 14:58:38 +0200

    reset dotfiles

--- dot_config/nvim/lua/lsp/mappings.lua
+++
@@ -1,9 +0,0 @@
--- Mappings.
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
--- dot_config/nvim/lua/plugins/vimgo.lua
+++
@@ -1,9 +0,0 @@
--- Vim go setup
-
-vim.g.go_fmt_command = "goimports"
-
-
-local exec = vim.api.nvim_exec
-
--- Autocmd
-exec([[ autocmd BufNewFile,BufRead *.go setlocal noexpandtab tabstop=4 shiftwidth=4 ]], false)
