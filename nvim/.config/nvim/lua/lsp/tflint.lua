bfe7a1458897691a07cf022c61061b768289311c
Alain Anghelidi <alainanghelidi@gmail.com>
2023-09-30 23:28:50 +0200

    enh: remove unused terraform config

--- nvim/.config/nvim/lua/lsp/tflint.lua
+++
@@ -1,9 +0,0 @@
--- Tflint setup
-local utils = require('lsp.utils')
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require('lspconfig')['tflint'].setup {
-  on_attach = utils.on_attach,
-  capabilities = capabilities,
-}
