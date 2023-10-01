a54b8d9df13893c6976f14b8cae42d6e03dbd93e
Alain Anghelidi <alainanghelidi@gmail.com>
2023-10-01 11:09:57 +0200

    fix: unable syntax highlighting with treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = {"bash", "cmake", "comment", "dockerfile", "go", "html", "javascript", "markdown", "python", "regex", "toml", "tsx", "typescript", "yaml", "lua" },
   auto_install = true,
   highlight = {
+    enable = true,
     additional_vim_regex_highlighting = false,
   },
 }
