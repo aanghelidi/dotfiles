bfeb3441a8761a42cf2d910f809c4ca94b6fa4fa
Alain Anghelidi <alainanghelidi@gmail.com>
2024-08-16 19:59:00 +0200

    enh: move from pyenv to hatch

--- bash/bashrc.d/config/python.sh
+++ bash/bashrc.d/config/python.sh
@@ -1,8 +1,6 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
-
-# Pipx
-export PATH="$PATH:/home/aangel/.local/bin"
-eval "$(register-python-argcomplete pipx)"
+# python will called 3.12 by default
+export PATH="$PATH:/home/aangel/.local/share/hatch/pythons/3.12/python/bin"
+export PATH="$PATH:/home/aangel/.local/share/hatch/pythons/3.11/python/bin"
+export PATH="$PATH:/home/aangel/.local/share/hatch/pythons/3.10/python/bin"
+export PATH="$PATH:/home/aangel/.local/share/hatch/pythons/3.9/python/bin"
+export PATH="$PATH:/home/aangel/.local/share/hatch/pythons/3.8/python/bin"
