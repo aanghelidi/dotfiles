a34bfd3ff1f9e311f676ac58bcb48d25c9715d67
Alain Anghelidi <alainanghelidi@gmail.com>
2024-08-25 10:30:33 +0200

    feat: hello uv

--- bash/bashrc.d/config/python.sh
+++ bash/bashrc.d/config/python.sh
@@ -1,6 +1,5 @@
-# python will called 3.12 by default
-export PATH="$PATH:/home/aangel/.local/share/hatch/pythons/3.12/python/bin"
-export PATH="$PATH:/home/aangel/.local/share/hatch/pythons/3.11/python/bin"
-export PATH="$PATH:/home/aangel/.local/share/hatch/pythons/3.10/python/bin"
-export PATH="$PATH:/home/aangel/.local/share/hatch/pythons/3.9/python/bin"
-export PATH="$PATH:/home/aangel/.local/share/hatch/pythons/3.8/python/bin"
+. "$HOME/.cargo/env"
+
+export PATH="$PATH:/home/aangel/.local/share/uv/python/cpython-3.12.5-linux-x86_64-gnu/bin"
+export PATH="$PATH:/home/aangel/.local/share/uv/python/cpython-3.10.14-linux-x86_64-gnu/bin"
+export PATH="$PATH:/home/aangel/.local/share/uv/python/cpython-3.9.19-linux-x86_64-gnu/bin/"
